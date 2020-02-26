74071c915a9da65d6d6b0542c4ea8ffad18ef651
Dronch <dr0shik90@gmail.com>
2020-02-26 12:15:09 +0300

    config replaced

---
+++ config.py
@@ -0,0 +1,13 @@
+WEB_EXTRACTOR_SOURCE = 'exchangeratesapi.io'
+WEB_EXTRACTOR_TIMEOUT = 5
+
+DATABASE_SQLALCHEMY_URL = 'sqlite://///home/ubuntu/PycharmProjects/telegram-change-bot/app.db'
+
+CACHE_TIMEOUT = 60 * 10
+
+BASE_CURRENCY = 'USD'
+
+TELEGRAM_TOKEN = '1066789665:AAGKAKh-0JPURkTbSKj0pCU7zmZMZ_d75gc'
+TELEGRAM_REQUEST_KWARGS = {
+    'proxy_url': 'socks5://localhost:9050'
+}
